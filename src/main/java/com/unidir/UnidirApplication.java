5f76c8276a69c44f45cb34c19b40e4ccf45081c3
ArchitB17976 <symbolra@gmail.com>
2022-08-06 17:07:22 +0800

    Project init

---
+++ src/main/java/com/unidir/UnidirApplication.java
@@ -0,0 +1,13 @@
+package com.unidir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidirApplication.class, args);
+	}
+
+}
